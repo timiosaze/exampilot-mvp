351ecb6ebf3c3deb3ba4b7fe1168600caf803abb
timiosaze <timiade1993@gmail.com>
2025-11-07 10:25:53 +0100

    Last edited commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    flowbiteReact(),
-    nodePolyfills({
-      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
-      include: ["fs"],
-    }),
-  ],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
   server: {
     proxy: {
       "/api": {
